1e6a67adf18b1e945d3f1a088223fb5c6d2dcf4b
AtharvaPC <baodhankaratharva@gmail.com>
2025-02-11 19:25:40 +0530

    Last

--- config.example.js
+++ config.example.js
@@ -5,7 +5,8 @@ const firebaseConfig = {
     projectId: "YOUR_PROJECT_ID",
     storageBucket: "YOUR_STORAGE_BUCKET",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
 };
 
 export default firebaseConfig; 
